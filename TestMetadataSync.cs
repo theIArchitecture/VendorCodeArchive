0e02538d37554c36e84dba59c9ed612d89201d6d
Mike Jensen <mjensen2k@gmail.com>
2025-12-10 10:45:12 -0700

    test: Week 4 metadata sync final validation
    Tests all fixes:
    - skip ci prevents double workflow run
    - PAT allows metadata push to DemoRepo main
    - Integer comparison blocks PR properly
    - Bot-check prevents infinite loops

---
+++ TestMetadataSync.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace VendorCode.Tests
+{
+    /// <summary>
+    /// Test file for Week 4 metadata sync workflow validation.
+    /// Contains intentional MD5 violation for AWS-FIPS-CRYPTO-001 rule.
+    /// </summary>
+    public class TestMetadataSync
+    {
+        public string GenerateHash(string input)
+        {
+            // This will trigger AWS-FIPS-CRYPTO-001 violation (MD5 is not FIPS-compliant)
+            using (var md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+                return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+            }
+        }
+    }
+}
